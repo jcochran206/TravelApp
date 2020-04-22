3fd9e54349637710e79029e9bf98c6227e193910
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-04-21 19:04:43 -0700

    added map function and added map

--- js/jonapp.js
+++ js/jonapp.js
@@ -1,6 +1,7 @@
 
 //api id goes here 
 const unsplash_client_id = 'MFxLwucLgtyOgI_LiCObo9cJX-Sn-GUcDPkjQM9ToJ4';
+const google_id = 'AIzaSyBgn6LAdcnAavmT1BAsiSMczJjQWRWbUOg'; 
 
 //function gets the photos from unsplash api
 function getPhotos(searchterm) {
@@ -12,29 +13,51 @@ function getPhotos(searchterm) {
     .catch(error => console.log(error));
 }
 
+//function to build map on screen
+function initMap() {
+    var options = {
+        center: {lat: 47.6062, lng: -122.3321},
+        zoom: 8
+    };
+
+   let map = new google.maps.Map(document.getElementById('map'), options);
+
+   let marker = new google.maps.Marker({
+       position: {lat: 47.6062, lng: -122.3321},
+       map:map
+   });
+
+   let infoWindow = new google.maps.InfoWindow({
+       content: '<h3> Seattle </h3>'
+   });
+
+   marker.addListener('click', function(){
+       infoWindow.open(map, marker);
+   })
+}
+
+
 //display content on screen
 function displayResults(responseJson) {
-    console.log(responseJson.results);
-    
-
+    //console.log(responseJson.results);
+    //uses for each jquery function 
     $.each(responseJson.results, function(i,val){
-        //console.log(i,val)
+        //console.log(i,val) review functions
         let imageUrl = val.urls.thumb;
         console.log(imageUrl);
         $('.images').append(`<img class="results_img" src="${imageUrl}"/>`);
     })
-
-    
-
   }
 
 
 // obtains user input and submit on form
 function watchForm(){
+    //watchs for form submission 
     $('.search-bar').submit(e => {
         e.preventDefault();
         let searchterm = $('#search').val();
         console.log(searchterm);
+        //calls get photos function 
         getPhotos(searchterm);
     })
 }
@@ -42,7 +65,6 @@ function watchForm(){
 /* function that contains all forms for app to use*/
 function main(){
     watchForm();
-   //displayResults();
 }
 
 
